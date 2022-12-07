2c3471568588987f684b87909d092f2d794f006a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-12-06 23:33:20 -0300

    Finaliza desafio

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,5 +1,17 @@
+require Integer
+
 defmodule ListFilter do
-  def hello do
-    :world
+  def call(list) do
+    numbers = numbers_only(list)
+
+    odds =
+      Enum.filter(numbers, fn x ->
+        number = String.to_integer(x)
+        Integer.is_odd(number)
+      end)
+
+    Enum.count(odds)
   end
+
+  defp numbers_only(list), do: Enum.filter(list, fn x -> Integer.parse(x) != :error end)
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,19 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  test "count the odd numbers properly" do
+    expected_number = 2
+
+    response = ListFilter.call(["1", "3", "banana", "56"])
+
+    assert response == expected_number
+  end
+
+  test "count 0 when no integer text representantion was found" do
+    expected_number = 0
+
+    response = ListFilter.call(["banana", "apple"])
+
+    assert response == expected_number
   end
 end
